f6acf50f894910986dbbe0879b9375687e44520d
ortigosa1971 <eliasalfarnate@gmail.com>
2025-04-04 18:52:27 +0200

    Subida inicial de la estación meteorológica

---
+++ server.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post('/send-email', (req, res) => {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'eliasalfarnate@gmail.com',
+    subject: 'Solicitud de acceso',
+    text: 'Un usuario ha solicitado acceso a la estación meteorológica.',
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Correo enviado: ' + info.response);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
